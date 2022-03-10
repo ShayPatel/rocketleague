cf2b922c42352758fe0db4321f80d22a09bdbed2
Akshay Patel <apatel1995@hotmail.com>
2022-03-09 18:22:52 -0600

    added the state setter in the environment. added a test function to see different output values. using the chase ball reward

--- rocketleague.py
+++ rocketleague.py
@@ -1,26 +1,29 @@
 import numpy as np
 import rlgym
 from rlgym.utils.terminal_conditions.common_conditions import TimeoutCondition, GoalScoredCondition
-from rlgym.utils.state_setters import random_state
+from rlgym.utils.state_setters.random_state import RandomState
+from rlgym.utils.state_setters.default_state import DefaultState
 from rlgym.utils.obs_builders.advanced_obs import AdvancedObs
 from rlgym.utils.obs_builders.default_obs import DefaultObs
 from rlgym.utils.reward_functions.common_rewards import VelocityBallToGoalReward,LiuDistancePlayerToBallReward, FaceBallReward
 from rlgym.utils.action_parsers.default_act import DefaultAction
 from utils.action_parser import simple_action
+from utils.rewards import chase_ball_reward
 
 from stable_baselines3 import DDPG
 from stable_baselines3.common.noise import NormalActionNoise
 from stable_baselines3 import PPO
 
 
-def make_env(game_speed:int, action_parser=DefaultAction(), terminal_conditions=None, obs_builder=DefaultObs(), reward_function=LiuDistancePlayerToBallReward()):
+def make_env(game_speed:int, action_parser=DefaultAction(), terminal_conditions=None, obs_builder=DefaultObs(), reward_function=LiuDistancePlayerToBallReward(), state_setter=DefaultState()):
     if not terminal_conditions:
         env = rlgym.make(
             game_speed=game_speed,
             action_parser=action_parser,
             terminal_conditions=[TimeoutCondition(400),GoalScoredCondition()],
             obs_builder=obs_builder,
-            reward_fn=reward_function
+            reward_fn=reward_function,
+            state_setter=state_setter
         )
     else:
         env = rlgym.make(
@@ -28,11 +31,33 @@ def make_env(game_speed:int, action_parser=DefaultAction(), terminal_conditions=
             action_parser=action_parser,
             terminal_conditions=terminal_conditions,
             obs_builder=obs_builder,
-            reward_fn=reward_function
+            reward_fn=reward_function,
+            state_setter=state_setter
         )
 
     return env
 
+def test(env, model_name:str=None):
+    if model_name:
+        model = DDPG.load(model_name)
+    else:
+        model = PPO("MlpPolicy", env=env, verbose=1)
+
+    for i in range(20):
+        obs = env.reset()
+        done = False
+        while not done:
+            action, _states = model.predict(obs)
+
+            obs, reward, done, gameinfo = env.step(action)
+            #print("reward", reward)
+            #player = gameinfo["state"].players[0]
+            #print("position",player.car_data.position)
+            #print("velocity",player.car_data.linear_velocity)
+            #print("angular",player.car_data.angular_velocity)
+
+
+    env.close()
 
 
 def ddpg(model_name:str, env, training_timesteps:int):
@@ -84,7 +109,7 @@ if __name__ == "__main__":
     #Set to 1 to see the game played in real time
     game_speed = 1
     #maximum number of steps to run 
-    max_steps = 300
+    max_steps = 500
     #randomly sets the position of the ball and environment on reset
 
     #the advanced observation builder has a state of 76 values.
@@ -93,7 +118,8 @@ if __name__ == "__main__":
 
     #select the reward function to use
     #reward_function = LiuDistancePlayerToBallReward()
-    reward_function = VelocityBallToGoalReward()
+    #reward_function = VelocityBallToGoalReward()
+    reward_function = chase_ball_reward()
 
     #give the conditions to indicate if done
     terminal_conditions = [
@@ -105,19 +131,30 @@ if __name__ == "__main__":
     #action_parser = DefaultAction()
     action_parser = simple_action()
 
+    #defines the state at each restart
+    #state_setter = DefaultState()
+    state_setter = RandomState(
+        ball_rand_speed=True,
+        cars_rand_speed=True,
+        cars_on_ground=True
+    )
+
     env = make_env(
         game_speed=game_speed,
         action_parser=action_parser,
         terminal_conditions=terminal_conditions,
         obs_builder=obs_builder,
-        reward_function=reward_function
+        reward_function=reward_function,
+        state_setter=state_setter
     )
     
     #name of the model. used to load or save
-    model_name = "ddpg-ball-to-goal-velocity"
+    #model_name = "ddpg-ball-to-goal-velocity"
+    #model_name = "ddpg-player-to-ball-distance-final"
+    model_name = "ddpg-chase-ball-simple-action"
 
 
     #main()
-    #test()
-    ddpg(model_name, env, 2000000)
-    #eval_ddpg(model_name, env)
+    #test(env,model_name)
+    #ddpg(model_name, env, 4000000)
+    eval_ddpg(model_name, env)
